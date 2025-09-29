a9aed94e7094f4fb3284242905fcdc404a95dcae
HarisHerdiansyah <haris54237@gmail.com>
2025-09-29 10:42:13 +0700

    update false_position.cpp

--- newton-raphson/newton_raphson.cpp
+++ newton-raphson/newton_raphson.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+double f(const double x) {
+    return x*x*x - 6*x*x + 11*x - 6;
+}
+
+double df(const double x) {
+    return 3*x*x - 12*x + 11;
+}
+
+double newton_rapshon(double x, const double tol, const int max_iter) {
+    for (int i = 0; i < max_iter; i++) {
+        double fx = f(x);
+        double dfx = df(x);
+        if (std::fabs(fx) < tol) return x;
+        if (dfx == 0.0) throw std::domain_error("Derivative is zero. No solution found.");
+
+        double x_new = x - (fx / dfx);
+        if (std::fabs(x_new - x) < tol) return x_new;
+        x = x_new;
+    }
+
+    throw std::domain_error("No solution exists");
+}
 
 int main() {
     return 0;
