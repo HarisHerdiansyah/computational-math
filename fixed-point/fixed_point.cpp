b88beaa8b0db636557c4979cb0c55643b76ceca3
HarisHerdiansyah <haris54237@gmail.com>
2025-09-29 19:47:57 +0700

    update fixed_point.cpp

--- fixed-point/fixed_point.cpp
+++ fixed-point/fixed_point.cpp
@@ -2,10 +2,9 @@
 #include <cmath>
 #include <stdexcept>
 
-/*
 double f(const double x) {
     return x*x*x - 6*x*x + 11*x - 6;
-}*/
+}
 
 double g1(const double x) {
     return std::cbrt(6*x*x - 11*x + 6); // cbrt -> cube root -> akar pangkat tiga
@@ -31,6 +30,21 @@ double g3(const double x) {
 
 double dg3(const double x) {
     return (12*x - 3*x*x) / 11.0;
+}
+
+double fixed_point_iteration(double x, const double tol, const int max_iter) {
+    if (std::fabs(dg1(x)) >= 1) {
+        throw std::domain_error("Iteration is divergent.");
+    }
+
+    for (int i = 0; i < max_iter; i++) {
+        double x_new = g1(x);
+        if (std::fabs(x_new - x) < tol || std::fabs(f(x_new)) < tol)
+            return x_new;
+        x = x_new;
+    }
+
+    throw std::domain_error("Maximum iterations reached without convergence.");
 }
 
 int main() {
