c1565c702cea3d9d4f0661458cff22ce3b9d010d
HarisHerdiansyah <haris54237@gmail.com>
2025-10-01 19:40:12 +0700

    changes backward_substitution.cpp

--- backward-substitution/backward_substitution.cpp
+++ backward-substitution/backward_substitution.cpp
@@ -6,22 +6,22 @@
 typedef std::vector<double> row;
 typedef std::vector<row> matrix;
 
-std::vector<double> backward_substitution(const matrix &coef_mtx, const row &b_mtx) {
-    const int n = coef_mtx[0].size();
+std::vector<double> backward_substitution(const matrix &mtx) {
+    const int n = mtx[0].size() - 1;
     std::vector<double> solution(n);
     for (int i = n - 1; i >= 0; i--) {
         double sum = 0;
         for (int j = i + 1; j < n; j++) {
-            sum += coef_mtx[i][j] * solution[j];
+            sum += mtx[i][j] * solution[j];
         }
 
-        if (coef_mtx[i][i] == 0) {
-            if (b_mtx[i] - sum != 0) {
+        if (mtx[i][i] == 0) {
+            if (mtx[i][n] - sum != 0) {
                 throw std::domain_error("No solution exists.");
             }
             solution[i] = std::numeric_limits<double>::quiet_NaN();
         } else {
-            solution[i] = (b_mtx[i] - sum) / coef_mtx[i][i];
+            solution[i] = (mtx[i][n] - sum) / mtx[i][i];
         }
     }
 
